ca48260898bd93f76f5763c9d29443a8df598960
cstjohn <cameron.stjohn@ergon.com.au>
2014-11-12 11:03:00 +1000

    Initial Base Libraries

---
+++ Converters/Converters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace GlobalHelpers.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+
+    [ValueConversion(typeof(Dictionary<string, string>), typeof(string))]
+    public class GetDictionaryValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Dictionary<string, string>) && parameter is string) throw new NotSupportedException();
+            Dictionary<string, string> dict = (Dictionary<string, string>)value;
+            string outVal;
+            if (!dict.TryGetValue((string)parameter, out outVal)) outVal = null;
+            return outVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FunctionsGeneric/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalHelpers
+{
+    public static class Functions
+    {
+        public static string ExceptionToString(Exception e)
+        {
+            string temp;
+            temp = e.Source + Environment.NewLine + e.Message;
+            Exception ie = e.InnerException;
+            while (ie != null)
+            {
+                temp = temp + Environment.NewLine + Environment.NewLine
+                    + ie.Source + Environment.NewLine
+                    + ie.Message;
+                ie = ie.InnerException;
+            }
+            return temp;
+        }
+    }
+}
