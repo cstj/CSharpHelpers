ea7208140a07a4049de56cdf32e25535f1a27890
cstjohn <cameron.stjohn@ergon.com.au>
2015-10-23 16:52:02 +1000

    Added feature to call property changes without specifying a property name.  If this happens it uses the calling members name.

--- Helpers.MVVMHelpers/ViewModelBase.cs
+++ Helpers.MVVMHelpers/ViewModelBase.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Helpers.MVVMHelpers
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
         #region Property INotifyPropertyChanged
+        /// <summary>
+        /// For signaling that a property has changed on the class.  
+        /// Fire when any public property has changed or you want wachers to update.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void RaisePropertyChanged(string name)
+        /// <summary>
+        /// Raises a PropertyChanged event on the given Attribute.  
+        /// If no attribute is given it uses the name of the property that called this function. 
+        /// </summary>
+        /// <param name="propertyName">Name of property that changed.  If empty uses the calling property's name.</param>
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null)
             {
-                handler(this, new PropertyChangedEventArgs(name));
+                handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
         #endregion
