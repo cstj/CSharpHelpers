5bf02ca3656ea103bb48a5c303f3a2ac7d7ce2d0
cstjohn <cameron.stjohn@ergon.com.au>
2015-12-10 17:18:53 +1000

    Added item to encrypt config sections of a given .config file.

---
+++ ConfigSectionEncDec/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConfigSectionEncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) ToggleConfigEncryption(args[0]);
+        }
+
+        static void ToggleConfigEncryption(string exeConfigName)
+        {
+            // Takes the executable file name without the
+            // .config extension.
+            try
+            {
+                System.Configuration.ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap(); //Path to your config file
+                fileMap.ExeConfigFilename = exeConfigName;
+
+                // Open the configuration file and retrieve 
+                // the connectionStrings section.
+                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+                    
+                    //OpenExeConfiguration(exeConfigName);
+
+                ConnectionStringsSection section =
+                    config.GetSection("connectionStrings")
+                    as ConnectionStringsSection;
+
+                if (section.SectionInformation.IsProtected)
+                {
+                    // Remove encryption.
+                    section.SectionInformation.UnprotectSection();
+                }
+                else
+                {
+                    // Encrypt the section.
+                    section.SectionInformation.ProtectSection(
+                        "DataProtectionConfigurationProvider");
+                }
+                // Save the current configuration.
+                config.Save();
+
+                Console.WriteLine("Protected={0}",
+                    section.SectionInformation.IsProtected);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+
